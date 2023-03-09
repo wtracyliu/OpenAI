e6c06d48125588fb3617ba5b9ca2205be27a5111
Wentao Liu <wtracyliu@gmail.com>
2023-03-09 12:59:25 +0800

    test custom domain

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -270,8 +270,8 @@ internal extension OpenAI {
 
 internal extension URL {
 
-    static let completions = URL(string: "https://api.openai.com/v1/completions")!
-    static let images = URL(string: "https://api.openai.com/v1/images/generations")!
-    static let embeddings = URL(string: "https://api.openai.com/v1/embeddings")!
-    static let chats = URL(string: "https://api.openai.com/v1/chat/completions")!
+    static let completions = URL(string: "https://api.wtracyliu.com/v1/completions")!
+    static let images = URL(string: "https://api.wtracyliu.com/v1/images/generations")!
+    static let embeddings = URL(string: "https://api.wtracyliu.com/v1/embeddings")!
+    static let chats = URL(string: "https://api.wtracyliu.com/v1/chat/completions")!
 }
